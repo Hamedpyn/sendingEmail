d426bd1c535bff8de1b103b138c66b8988f0ab77
Hamed <hamedpyn@gmail.com>
2023-02-23 01:24:42 +0330

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
 // Variables
 
-const 
-subjectInput = document.querySelector('#subject'),
-formInput = document.querySelector('form'),
-emailInput = document.querySelector('#email'),
-messageInput = document.querySelector('#message'),
-sendBtn = document.querySelector('#sendBtn'),
-resetBtn = document.querySelector('#resetBtn');
+const
+    subjectInput = document.querySelector('#subject'),
+    formInput = document.querySelector('form'),
+    emailInput = document.querySelector('#email'),
+    messageInput = document.querySelector('#message'),
+    sendBtn = document.querySelector('#sendBtn'),
+    resetBtn = document.querySelector('#resetBtn');
 
 // Events
 document.addEventListener('DOMContentLoaded', init)
@@ -16,7 +16,7 @@ messageInput.addEventListener('blur', validateform)
 resetBtn.addEventListener('click', () => {
     formInput.reset()
 })
-formInput.addEventListener('click', (e) => {
+formInput.addEventListener('submit', (e) => {
     e.preventDefault
 })
 
@@ -24,42 +24,42 @@ formInput.addEventListener('click', (e) => {
 
 // TITLE : init
 // disabled sendBtn
-function init(){
+function init() {
     sendBtn.disabled = true
 }
 
 // TITLE : validateForm
-function validateform(){
-        // if input value isn't empty :
-    if ( this.value.length > 0 ){
-                // if input type is email :
-        if(this.type == 'email'){
-                        // if emailInput includes '@'
-            if(this.value.includes('@')){
+function validateform() {
+    // if input value isn't empty :
+    if (this.value.length > 0) {
+        // if input type is email :
+        if (this.type == 'email') {
+            // if emailInput includes '@'
+            if (this.value.includes('@')) {
                 // change color to green
-                changeColor(this , 'valid' , 'invalid')
-            }else{
+                changeColor(this, 'valid', 'invalid')
+            } else {
                 // if emailInput doesn't includes '@', change color red
-                changeColor(this , 'invalid' , 'valid')
+                changeColor(this, 'invalid', 'valid')
             }
-        }else{
-                        // if input type is not email, change color to green
-            changeColor(this , 'valid' , 'invalid')
+        } else {
+            // if input type is not email, change color to green
+            changeColor(this, 'valid', 'invalid')
         }
-    }else{
-        
+    } else {
+
         // if input value is empty , change color to red
-        changeColor(this , 'invalid' , 'valid')
+        changeColor(this, 'invalid', 'valid')
     }
     checkSendBtnStatus()
 }
 
 // TITLE : check sendBtn status
-function checkSendBtnStatus(){
+function checkSendBtnStatus() {
     // if email
-    if( emailInput.classList.contains('valid') && subjectInput.classList.contains('valid') && messageInput.classList.contains('valid') ){
+    if (emailInput.classList.contains('valid') && subjectInput.classList.contains('valid') && messageInput.classList.contains('valid')) {
         sendBtn.disabled = false
-    }else{
+    } else {
         sendBtn.disabled = true
     }
 }
@@ -68,7 +68,7 @@ function checkSendBtnStatus(){
 // el : add and remove class in itself
 // prev : name of the class to add in el
 // next : name of the class to remove from el
-function changeColor( el , prev , next ){
+function changeColor(el, prev, next) {
     el.classList.add(prev)
     el.classList.remove(next)
 }
